9226594cb2a18dcd811338b3fc126f8d97c3fd64
occy88 <octavio.delser@gmail.com>
2025-02-22 12:50:53 +0000

    Remove sample_app

--- sample_app/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class SampleAppConfig(AppConfig):
-    name = "sample_app"
-
-    # def ready(self):
-    #     # load receivers
-    #     import sample_app.receivers  # noqa F401
--- sample_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from django_toolkit.db.models import UpdatableModel
-
-
-class MyModelManager(models.Manager): ...
-
-
-class MyModel(UpdatableModel): ...
